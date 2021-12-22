30b584aa623ce4e90db0beac96d7d9993b8f5c2e
zqwerty <zhuq96@hotmail.com>
2021-12-22 03:27:12 +0000

    modify load_dataset interface

---
+++ convlab2/util/__init__.py
@@ -0,0 +1,2 @@
+from convlab2.util.unified_datasets_util import load_dataset, load_ontology, load_database, \
+    load_unified_data, load_nlu_data, load_dst_data, load_policy_data, load_nlg_data, load_e2e_data
