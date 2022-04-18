d4a98242b2c1b92ee7574a9e0161cc8b7580a576
zqwerty <zhuq96@hotmail.com>
2022-04-18 10:38:32 +0800

    update source_prefix args in run_seq2seq.py, update create_data.py for t5: concat utt with \n token

--- convlab2/base_models/t5/create_data.py
+++ convlab2/base_models/t5/create_data.py
@@ -15,9 +15,10 @@ def create_rg_data(dataset, data_dir, args):
     for data_split in data_splits:
         data = []
         for sample in tqdm(data_by_split[data_split], desc=f'{data_split} sample', leave=False):
-            context = ' '.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']])
-            response = f"{sample['speaker']}: {sample['utterance']}"
-            data.append(json.dumps({'context': context, 'response': response}, ensure_ascii=False)+'\n')
+            if len(sample['context']) == 0:
+                continue
+            context = '\n'.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']]+[f'{sample["speaker"]}: '])
+            data.append(json.dumps({'context': context, 'response': sample['utterance']}, ensure_ascii=False)+'\n')
 
         file_name = os.path.join(data_dir, f"{data_split}.json")
         with open(file_name, "w", encoding='utf-8') as f:
@@ -34,7 +35,7 @@ def create_nlu_data(dataset, data_dir, args):
         for sample in tqdm(data_by_split[data_split], desc=f'{data_split} sample', leave=False):
             response = f"{sample['speaker']}: {sample['utterance']}"
             if args.context_window_size>0:
-                context = ' '.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']]+[response])
+                context = '\n'.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']]+[response])
             else:
                 context = response
             dialogue_acts_seq = serialize_dialogue_acts(sample['dialogue_acts'])
@@ -56,12 +57,34 @@ def create_dst_data(dataset, data_dir, args):
         for sample in tqdm(data_by_split[data_split], desc=f'{data_split} sample', leave=False):
             response = f"{sample['speaker']}: {sample['utterance']}"
             if args.context_window_size>0:
-                context = ' '.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']]+[response])
+                context = '\n'.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']]+[response])
             else:
                 context = response
             state_seq = serialize_dialogue_state(sample['state'])
             assert equal_state_seq(sample['state'], state_seq), print(sample['state'], state_seq, deserialize_dialogue_state(state_seq))
             data.append(json.dumps({'context': context, 'state_seq': state_seq}, ensure_ascii=False)+'\n')
+
+        file_name = os.path.join(data_dir, f"{data_split}.json")
+        with open(file_name, "w", encoding='utf-8') as f:
+            f.writelines(data)
+
+def create_nlg_data(dataset, data_dir, args):
+    data_by_split = load_nlu_data(dataset, speaker=args.speaker, use_context=args.context_window_size>0, context_window_size=args.context_window_size)
+    data_dir = os.path.join(data_dir, args.speaker, f'context_{args.context_window_size}')
+    os.makedirs(data_dir, exist_ok=True)
+
+    data_splits = data_by_split.keys()
+    for data_split in data_splits:
+        data = []
+        for sample in tqdm(data_by_split[data_split], desc=f'{data_split} sample', leave=False):
+            dialogue_acts_seq = serialize_dialogue_acts(sample['dialogue_acts'])
+            if args.context_window_size>0:
+                context = '\n'.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['context']]+[f'{sample["speaker"]}: '])
+                context = f'{dialogue_acts_seq}\n\n{context}'
+            else:
+                context = f'{dialogue_acts_seq}\n\n{sample["speaker"]}: '
+            assert equal_da_seq(sample['dialogue_acts'], dialogue_acts_seq), print(sample['dialogue_acts'], dialogue_acts_seq, deserialize_dialogue_acts(dialogue_acts_seq))
+            data.append(json.dumps({'context+da': context, 'response': sample['utterance']}, ensure_ascii=False)+'\n')
 
         file_name = os.path.join(data_dir, f"{data_split}.json")
         with open(file_name, "w", encoding='utf-8') as f:
@@ -76,7 +99,7 @@ def create_goal2dialogue_data(dataset, data_dir, args):
         data = []
         for sample in tqdm(data_by_split[data_split], desc=f'{data_split} sample', leave=False):
             goal = re.sub(r'<.*?>', '', sample['goal']['description'])
-            dialogue = ' '.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['turns']])
+            dialogue = '\n'.join([f"{turn['speaker']}: {turn['utterance']}" for turn in sample['turns']])
             data.append(json.dumps({'goal': goal, 'dialogue': dialogue}, ensure_ascii=False)+'\n')
 
         file_name = os.path.join(data_dir, f"{data_split}.json")
@@ -87,7 +110,7 @@ def create_goal2dialogue_data(dataset, data_dir, args):
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser(description="create data for seq2seq training")
-    parser.add_argument('--tasks', '-t', metavar='task_name', nargs='*', choices=['rg', 'nlu', 'dst', 'goal2dialogue'], help='names of tasks')
+    parser.add_argument('--tasks', '-t', metavar='task_name', nargs='*', choices=['rg', 'nlu', 'dst', 'nlg', 'goal2dialogue'], help='names of tasks')
     parser.add_argument('--datasets', '-d', metavar='dataset_name', nargs='*', help='names of unified datasets')
     parser.add_argument('--speaker', '-s', type=str, choices=['user', 'system', 'all'], help='speaker(s)')
     parser.add_argument('--context_window_size', '-c', type=int, default=0, help='how many contextual utterances are considered')
