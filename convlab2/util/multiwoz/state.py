e095e04388a0699dfbc45b20b7398a873719a365
Hsien-Chin Lin <linh@hhu.de>
2022-04-24 23:18:23 +0200

    unify dataformat for TUS

--- convlab2/util/multiwoz/state.py
+++ convlab2/util/multiwoz/state.py
@@ -7,3 +7,94 @@ def default_state():
                  terminated=False,
                  history=[])
     return state
+
+
+def default_state_old():
+    state = dict(user_action=[],
+                 system_action=[],
+                 belief_state={},
+                 request_state={},
+                 terminated=False,
+                 history=[])
+    state['belief_state'] = {
+        "police": {
+            "book": {
+                "booked": []
+            },
+            "semi": {}
+        },
+        "hotel": {
+            "book": {
+                "booked": [],
+                "people": "",
+                "day": "",
+                "stay": ""
+            },
+            "semi": {
+                "name": "",
+                "area": "",
+                "parking": "",
+                "pricerange": "",
+                "stars": "",
+                "internet": "",
+                "type": ""
+            }
+        },
+        "attraction": {
+            "book": {
+                "booked": []
+            },
+            "semi": {
+                "type": "",
+                "name": "",
+                "area": ""
+            }
+        },
+        "restaurant": {
+            "book": {
+                "booked": [],
+                "people": "",
+                "day": "",
+                "time": ""
+            },
+            "semi": {
+                "food": "",
+                "pricerange": "",
+                "name": "",
+                "area": "",
+            }
+        },
+        "hospital": {
+            "book": {
+                "booked": []
+            },
+            "semi": {
+                "department": ""
+            }
+        },
+        "taxi": {
+            "book": {
+                "booked": []
+            },
+            "semi": {
+                "leaveAt": "",
+                "destination": "",
+                "departure": "",
+                "arriveBy": ""
+            }
+        },
+        "train": {
+            "book": {
+                "booked": [],
+                "people": ""
+            },
+            "semi": {
+                "leaveAt": "",
+                "destination": "",
+                "day": "",
+                "arriveBy": "",
+                "departure": ""
+            }
+        }
+    }
+    return state
