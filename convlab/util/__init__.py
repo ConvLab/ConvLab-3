7d5388bb8b357120aaf20149c86aa8d2a5b678d7
zqwerty <zhuq96@hotmail.com>
2022-06-29 10:04:24 +0800

    add split2ratio arg in load_dataset

--- convlab/util/__init__.py
+++ convlab/util/__init__.py
@@ -1,3 +1,1 @@
-from convlab.util.unified_datasets_util import load_dataset, load_ontology, load_database, \
-    load_unified_data, load_nlu_data, load_dst_data, load_policy_data, load_nlg_data, load_e2e_data, load_rg_data, \
-    download_unified_datasets, relative_import_module_from_unified_datasets
+from convlab.util.unified_datasets_util import *
