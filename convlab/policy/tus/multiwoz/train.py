2e08ba8594509f2f53d20b36ae5ba4100577aed5
Hsien-Chin Lin <linh@hhu.de>
2022-07-18 23:45:24 +0200

    add analysis

--- convlab/policy/tus/multiwoz/train.py
+++ convlab/policy/tus/multiwoz/train.py
@@ -216,6 +216,8 @@ if __name__ == "__main__":
 
     batch_size = config["batch_size"]
 
+    # load data with "load_data"
+
     # check train/test data
     data = {"train": {}, "test": {}}
     for data_type in data:
