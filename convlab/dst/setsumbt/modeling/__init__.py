06491f2bc1ccce94f65f5fa5bba287fcec80bd6a
Carel van Niekerk <niekerk@hhu.de>
2022-08-04 10:48:56 +0000

    Update convlab/dst/setsumbt/modeling/__init__.py

--- convlab/dst/setsumbt/modeling/__init__.py
+++ convlab/dst/setsumbt/modeling/__init__.py
@@ -1,5 +1,5 @@
 from convlab.dst.setsumbt.modeling.bert_nbt import BertSetSUMBT
 from convlab.dst.setsumbt.modeling.roberta_nbt import RobertaSetSUMBT
-from convlab.dst.setsumbt.modeling.ensemble_nbt import EnsembleSetSUMBT, DropoutEnsembleSetSUMBT
+from convlab.dst.setsumbt.modeling.ensemble_nbt import EnsembleSetSUMBT
 
 from convlab.dst.setsumbt.modeling.temperature_scheduler import LinearTemperatureScheduler
