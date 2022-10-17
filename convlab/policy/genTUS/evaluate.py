a5396e2800988b91803741647cd179332d47d0ab
Hsien-Chin Lin <linh@hhu.de>
2022-10-17 14:34:21 +0200

    wip

--- convlab/policy/genTUS/evaluate.py
+++ convlab/policy/genTUS/evaluate.py
@@ -135,7 +135,7 @@ class Evaluator:
         else:
             print("Calculate SER")
             missing, hallucinate, total, hallucination_dialogs, missing_dialogs = fine_SER(
-                nlg_eval["dialog"]["gen_acts"], nlg_eval["dialog"]["gen_utts"])
+                gen_r["gen_acts"], gen_r["gen_utts"])
 
             print("{} Missing acts: {}, Total acts: {}, Hallucinations {}, SER {}".format(
                 "genTUSNLG", missing, total, hallucinate, missing/total))
