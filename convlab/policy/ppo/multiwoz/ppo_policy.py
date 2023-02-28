1bad2af8172c532bf8821072fba3c415bb5e26ca
trtd56 <hoge@fuga.com>
2023-02-28 14:40:53 +0900

    fix load_from_pretrained args

--- convlab/policy/ppo/multiwoz/ppo_policy.py
+++ convlab/policy/ppo/multiwoz/ppo_policy.py
@@ -9,7 +9,5 @@ class PPOPolicy(PPO):
                         model_file="https://huggingface.co/ConvLab/ConvLab-2_models/resolve/main/ppo_policy_multiwoz.zip"
                         ):
         super().__init__(is_train=is_train, dataset=dataset)
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json'), 'r') as f:
-            cfg = json.load(f)
-        self.load_from_pretrained(archive_file, model_file, cfg['load'])
+        self.load_from_pretrained(model_file)
         
