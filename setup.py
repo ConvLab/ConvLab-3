49df2f1dc6e42fdf84d90285cc3931e231babb14
zqwerty <zhuq96@hotmail.com>
2023-05-29 10:05:18 +0800

    rm deepspeech package since it is no longer maintained and cause installation error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='convlab',
-    version='3.0.1',
+    version='3.0.2a',
     packages=find_packages(),
     license='Apache',
     description='An Open-source Dialog System Toolkit',
@@ -55,7 +55,6 @@ setup(
         'fuzzywuzzy',
         'json_lines',
         'gtts',
-        'deepspeech',
         'pydub'
     ],
     extras_require={
