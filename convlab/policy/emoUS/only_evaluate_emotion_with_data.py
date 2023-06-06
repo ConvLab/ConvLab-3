079a75c66a5bfcaba5d93daae74fa1f6211291ca
Hsien-Chin Lin <linh@hhu.de>
2023-06-06 14:25:32 +0200

    wip

--- convlab/policy/emoUS/only_evaluate_emotion_with_data.py
+++ convlab/policy/emoUS/only_evaluate_emotion_with_data.py
@@ -5,6 +5,7 @@ import json
 import torch
 from convlab.policy.emoUS.emoUS import UserActionPolicy
 import os
+from convlab.policy.emoUS.evaluate import emotion_score
 
 
 def arg_parser():
@@ -15,10 +16,12 @@ def arg_parser():
                         default="convlab/policy/emoUS/unify/data/language_emowoz+dialmage_0_1/test.json")
     parser.add_argument(
         "--model", type=str, default="convlab/policy/emoUS/unify/experiments/EmoUS_default/EmoUS_default")
+    parser.add_argument("--whole", action="store_true",
+                        help="evaluate the whole test set")
     return parser.parse_args()
 
 
-def evaluate(sent_id, text, test_data, model_checkpoint):
+def one_sent_evaluate(sent_id, text, test_data, model_checkpoint):
     if sent_id is None:
         sent_id = randint(0, len(test_data))
     in_json = json.loads(test_data[sent_id]["in"])
@@ -48,11 +51,29 @@ def generate(model: UserActionPolicy, in_json, text=None):
     print(emotion)
 
 
+def whole_evaluate(test_data, model_checkpoint):
+    model = load_model(model_checkpoint)
+    preds = []
+    labels = []
+    for data in test_data:
+        in_json = json.loads(data["in"])
+        out_json = json.loads(data["out"])
+        pred = generate(model, in_json)
+        preds.append(pred["emotion"])
+        labels.append(out_json["emotion"])
+    r = emotion_score(golden_emotions=labels,
+                      gen_emotions=preds, dirname=model_checkpoint)
+    print(r)
+
+
 def main():
     args = arg_parser()
     with open(args.test_file) as f:
         test_data = json.load(f)
-    evaluate(args.id, args.text, test_data["dialog"], args.model)
+    if args.whole:
+        whole_evaluate(test_data["dialog"], args.model)
+    else:
+        one_sent_evaluate(args.id, args.text, test_data["dialog"], args.model)
 
 
 if __name__ == "__main__":
