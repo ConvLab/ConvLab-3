24fce2d19e930b1b82bd24d8ff3d5a9f33f0644d
Hsien-Chin Lin <linh@hhu.de>
2023-06-13 15:42:50 +0200

    wip

--- convlab/policy/emoUS/train_model_llm.py
+++ convlab/policy/emoUS/train_model_llm.py
@@ -53,7 +53,7 @@ def get_model(model_path):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     # tokenizer.add_tokens(["<?>"])
-    model = AutoModelForSeq2SeqLM.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         model_path, torch_dtype=torch.float16)
     if torch.cuda.is_available():
         print("use cuda")
