4a702c0f0c59e454b4dbafd0fede493419a4d358
Hsien-Chin Lin <linh@hhu.de>
2023-09-12 11:23:08 +0200

    debug

--- convlab/dialog_agent/session.py
+++ convlab/dialog_agent/session.py
@@ -126,6 +126,7 @@ class BiSession(Session):
         """
         if hasattr(self.sys_agent.policy, 'get_conduct'):
             sys_conduct = self.sys_agent.policy.get_conduct()
+            print("sys_conduct: ", sys_conduct)
             user_response = self.next_response(
                 last_observation, sys_conduct=sys_conduct)
         else:
