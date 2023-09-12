39c1f31d2fd8d3b82b28443db91262c93189baac
Hsien-Chin Lin <linh@hhu.de>
2023-09-12 11:18:39 +0200

    wip

--- convlab/policy/emoUS_v2/test.py
+++ convlab/policy/emoUS_v2/test.py
@@ -2,6 +2,10 @@ from argparse import ArgumentParser
 
 from convlab.dialog_agent import PipelineAgent
 from convlab.policy.emoUS_v2.semanticEmoUS import UserPolicy
+from convlab.policy.vector.vector_nodes import VectorNodes
+from convlab.policy.vtrace_DPT import VTRACE
+from convlab.dst.rule.multiwoz.dst import RuleDST
+from convlab.dialog_agent.session import BiSession
 
 
 def arg_parser():
@@ -27,6 +31,22 @@ def test():
     usr.init_session()
     print(usr.response([], sys_conduct="neutral"))
     print(usr.response([], sys_conduct="enthusiastic"))
+
+    vectorizer = VectorNodes(dataset_name='multiwoz21',
+                             use_masking=True,
+                             manually_add_entity_names=True,
+                             seed=0,
+                             filter_state=True)
+    sys_policy = VTRACE(is_train=False,
+                        seed=0,
+                        vectorizer=vectorizer,
+                        load_path="from_pretrained")
+    # test for seestion
+    dst = RuleDST()
+    sys = PipelineAgent(None, dst, sys_policy, None, name='sys')
+    sess = BiSession(sys, usr)
+    x = sess.next_turn([])
+    print(x)
 
 
 if __name__ == "__main__":
