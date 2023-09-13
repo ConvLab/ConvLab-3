small bug fix in input of rule-based simulator and emotion conversion to string. Added emotion return calculation during evaluation

convlab/policy/vtrace_DPT/transformer_model/EncoderDecoder.py
@@ -266,7 +266,7 @@ class EncoderDecoder(nn.Module):
             action_mask_list.append(action_mask)
             action_list_num.append(emotion)
 
-        self.info_dict["conduct"] = emotion
+        self.info_dict["conduct"] = self.action_embedder.small_action_dict_reversed[emotion]
         self.info_dict["kg"] = kg_list[0]
         self.info_dict["small_act"] = torch.Tensor(action_list_num)
         self.info_dict["action_mask"] = torch.stack(action_mask_list)