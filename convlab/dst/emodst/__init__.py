d864d27d973623b8b3c25d2f82a1c46c8aeb6c1a
Shutong Feng <shutong.feng@hhu.de>
2023-10-24 13:18:52 +0000

    removed comment. Trippy also tested

--- convlab/dst/emodst/__init__.py
+++ convlab/dst/emodst/__init__.py
@@ -1,9 +1,1 @@
 from convlab.dst.emodst.tracker import EMODST
-
-# # supported erc
-# # from convlab.dst.emodst.modeling.emotion_estimator import BertERC
-
-# SUPPORTED_DST = {
-#     'setsubmt': SetSUMBTTracker,
-#     'trippy': TRIPPY
-# }
