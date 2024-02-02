aee2b3c6b6ef446f422222ecc45c2d880a27537a
Shutong Feng <shutong.feng@hhu.de>
2024-02-02 15:45:34 +0100

    dev for emotod interactive evaluation

---
+++ convlab/e2e/emotod/run_interaction.py
@@ -0,0 +1,24 @@
+from convlab.util.analysis_tool.analyzer import Analyzer, PipelineAgent
+
+from convlab.e2e.emotod.emotod import EMOTODAgent
+from convlab.policy.emoUS.emoUS import UserPolicy
+from convlab.nlu.jointBERT.multiwoz.nlu import BERTNLU
+
+sys_nlu = None
+sys_dst = None
+sys_policy = EMOTODAgent(model_file='/home/fengs/projects/pretrained_models/gpt2_prev_emo')
+sys_nlg = None
+
+sys_agent = PipelineAgent(sys_nlu, sys_dst, sys_policy, sys_nlg, name='sys')
+
+
+user_nlu = BERTNLU(mode='sys', config_file='multiwoz_sys_context.json', model_file='/home/fengs/projects/pretrained_models/bert_multiwoz_sys_context.zip')
+user_dst = None
+user_policy = UserPolicy(model_checkpoint='/home/fengs/projects/pretrained_models/emous_lang')
+user_nlg = None
+
+user_agent = PipelineAgent(user_nlu, user_dst, user_policy, user_nlg, name='user')
+
+analyzer = Analyzer(user_agent=user_agent, dataset='multiwoz')
+
+analyzer.comprehensive_analyze(sys_agent=sys_agent, model_name='emotod_gpt2-emous_lang', total_dialog=250)
