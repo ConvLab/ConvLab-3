4e9ae07cc2a592a7f13f9d153267ce077269f302
Shutong Feng <shutong.feng@hhu.de>
2024-02-04 00:09:39 +0100

    run multiple seeds

--- convlab/e2e/emotod/run_interaction.py
+++ convlab/e2e/emotod/run_interaction.py
@@ -9,35 +9,36 @@ from convlab.base_models.t5.nlu.nlu import T5NLU
 
 from utils import seed_all
 
-seed_all(1)
+for seed in [1,2,3,4,5,6]:
+    seed_all(seed)
 
-sys_nlu = None
-sys_dst = None
-print('Initialising Emo-TOD')
-sys_policy = EMOTODAgent(model_file='/home/fengs/projects/pretrained_models/gpt2_prev_emo')
-sys_nlg = None
+    sys_nlu = None
+    sys_dst = None
+    print('Initialising Emo-TOD')
+    sys_policy = EMOTODAgent(model_file='/home/fengs/projects/pretrained_models/gpt2_prev_emo')
+    sys_nlg = None
 
-# sys_agent = PipelineAgent(sys_nlu, sys_dst, sys_policy, sys_nlg, name='sys')
-sys_agent = E2EAgentWrapper(sys_policy, 'emotod')
+    # sys_agent = PipelineAgent(sys_nlu, sys_dst, sys_policy, sys_nlg, name='sys')
+    sys_agent = E2EAgentWrapper(sys_policy, 'emotod')
 
-# user_nlu = BERTNLU(mode='sys', config_file='multiwoz_sys_context.json', model_file='/home/fengs/projects/pretrained_models/bert_multiwoz_sys_context.zip')
-print('Initialising T5NLU')
-user_nlu = T5NLU(speaker='system', context_window_size=3, model_name_or_path='/home/fengs/projects/pretrained_models/t5-small-nlu-all-multiwoz21-context3')
-user_dst = None
-print('Initialising EmoUS_Lang')
-emous_configs = {
-    'Neutral': 0.95,
-    'Satisfied': 0.95,
-    'sub_goal_succ': True
-}
-user_policy = UserPolicy(
-    model_checkpoint='/home/fengs/projects/pretrained_models/emous_lang', kwargs=emous_configs)
-user_nlg = None
+    # user_nlu = BERTNLU(mode='sys', config_file='multiwoz_sys_context.json', model_file='/home/fengs/projects/pretrained_models/bert_multiwoz_sys_context.zip')
+    print('Initialising T5NLU')
+    user_nlu = T5NLU(speaker='system', context_window_size=3, model_name_or_path='/home/fengs/projects/pretrained_models/t5-small-nlu-all-multiwoz21-context3')
+    user_dst = None
+    print('Initialising EmoUS_Lang')
+    emous_configs = {
+        'Neutral': 0.95,
+        'Satisfied': 0.95,
+        'sub_goal_succ': True
+    }
+    user_policy = UserPolicy(
+        model_checkpoint='/home/fengs/projects/pretrained_models/emous_lang', kwargs=emous_configs)
+    user_nlg = None
 
-user_agent = PipelineAgent(user_nlu, user_dst, user_policy, user_nlg, name='user')
+    user_agent = PipelineAgent(user_nlu, user_dst, user_policy, user_nlg, name='user')
 
-print('Initialising analyzer')
-analyzer = Analyzer(user_agent=user_agent, dataset='multiwoz')
+    print('Initialising analyzer')
+    analyzer = Analyzer(user_agent=user_agent, dataset='multiwoz')
 
-print('Start to analyze')
-analyzer.comprehensive_analyze(sys_agent=sys_agent, model_name='emotod_gpt2-emous_lang', total_dialog=250)
+    print('Start to analyze')
+    analyzer.comprehensive_analyze(sys_agent=sys_agent, model_name=f'emotod_gpt2-emous_lang-seed={str(seed)}', total_dialog=250)
