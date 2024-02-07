a034144cd3c07e246dd846726abab12485ac42f7
Shutong Feng <shutong.feng@hhu.de>
2024-02-07 14:36:37 +0100

    rename model to policy in the e2e wrapper

--- convlab/e2e/emotod/e2ewrapper.py
+++ convlab/e2e/emotod/e2ewrapper.py
@@ -3,14 +3,14 @@ from convlab.dialog_agent import Agent
 class E2EAgentWrapper(Agent):
     def __init__(self, e2e_model, name):
         super().__init__(name=name)
-        self.model = e2e_model
-        self.model.init_session()
+        self.policy = e2e_model
+        self.policy.init_session()
 
     def init_session(self):
-        self.model.init_session()
+        self.policy.init_session()
 
     def response(self, observation):
-        return self.model.response(observation)
+        return self.policy.response(observation)
 
     def get_in_da(self):
         return None
