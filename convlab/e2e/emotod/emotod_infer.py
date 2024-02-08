c74d48c78afbe32a68b02ea2baab4733210374cd
Shutong Feng <shutong.feng@hhu.de>
2024-02-08 13:43:48 +0100

    save to file

--- convlab/e2e/emotod/emotod_infer.py
+++ convlab/e2e/emotod/emotod_infer.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import json
+from copy import deepcopy
 
 from emotod import EMOTODAgent
 from utils import lexcalise
 
 with open('corpus_eval/emo_prev.json', 'r') as f:
     dataset = json.load(f)
+
+dataset_out = deepcopy(dataset)
 
 s = EMOTODAgent(model_file='/home/fengs/projects/pretrained_models/gpt2_prev_emo')
 
@@ -33,7 +36,8 @@ for split in ['train', 'valid', 'test']:
             full_generation = tokenizer.decode(outputs[0], skip_special_tokens=False)
 
             lex_resp = lexcalise(full_generation, s.database)
-            print(lex_resp)
-            exit()
+            dataset_out[split][dial_id][i]['output_delex'] = full_generation
+            dataset_out[split][dial_id][i]['output_lex'] = lex_resp
 
-
+with open('corpus_eval/multiwoz21_emotod_resp.json', 'w') as f:
+    json.dump(dataset_out, indent=4)
